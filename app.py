6d11967da017296214ed2a5594dcc00d23b18fd3
Gabriel Rodrigues <gabrielderodrigues@hotmail.com>
2024-10-23 14:35:49 -0300

    feat: Creating function returning data

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Hello, world!')
+import requests
+
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+
+response = requests.get(url)
+print(response)
+
+if response.status_code == 200:
+    print('Requisição bem sucedida')
+    print(response.json())
+else:
+    print(f'Erro na requisição | Status {response.status_code}')
